48bf31f291706294abbd65e17a1b12211a3dc624
RedopVago <juan.valenzuela@cinvestav.mx>
2021-03-17 17:40:01 -0600

    Comentarios Proyecto

--- cliente.py
+++ cliente.py
@@ -1,4 +1,4 @@
-from Estudiante import Estudiante as est
+from estudiante import Estudiante as est
 from PySide2.QtWidgets import QApplication
 from PySide2.QtWidgets import QMainWindow
 import sys
@@ -28,13 +28,16 @@ class Estudiante:
 
     def getContra(self):
         return self.__contra
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # [PV] Crear el objeto en el constructor y del tipo TCP. (SOCK_STREAM)
 class PaseLista(QMainWindow):
 
     def __init__(self):
         super(PaseLista, self).__init__() #Constructor de la clase padre
         self.ui = Ui_PaseLista() #se inicializa la variable ui asignando la clase que se generó en el archivo ui
         self.ui.setupUi(self) #se asigna la variable ui al mainWindow
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # [PV] Objeto socket perteneciente a la clase
+
         self.ui.desconectarb.setEnabled(False)
         self.ui.enviarb.setEnabled(False)
         self.ui.buscarE.setEnabled(False) #Provar con setRead()
@@ -50,7 +53,10 @@ class PaseLista(QMainWindow):
         self.ui.enviarb.clicked.connect(self.enviarDatos)
         self.ui.buscarb.clicked.connect(self.buscarArchivo)
         self.ui.buscarE.textEdited.connect(self.rutaEnviar)
-        self.ui.buscarb.clicked.connect(self.enviar)
+        # self.ui.buscarb.clicked.connect(self.enviar)  # [PV] El evento del boton archivosb es que que debe llamar el metodo enviar()
+        self.ui.archivosb.clicked.connect(self.enviar)
+
+        # self.ui.archivosb.clicked.connect(self.rutaEnviar) # [PV] No es necesario
         self.ui.desconectarb.clicked.connect(self.cerrarConexion)
 
 
@@ -74,8 +80,9 @@ class PaseLista(QMainWindow):
     def conexion(self):
         # host = 'localhost'
         host = '3.16.226.150'
-        port = 9999
-        s.connect((host, port))
+        # port = 9999
+        port = 9997  # [PV] para el servidor de chunks el puerto es 9997
+        self.s.connect((host, port))  # [PV] para facilidad de uso crear el socket en el constructor y usar self para que pertenezaca a la clase
         self.ui.nombreE.setEnabled(True)
         self.ui.correoE.setEnabled(True)
         self.ui.contraE.setEnabled(True)
@@ -95,41 +102,43 @@ class PaseLista(QMainWindow):
         estudiante = Estudiante(nombre, correo, contra)
         estudiante_seriado = pickle.dumps(estudiante)
         if isinstance(estudiante,Estudiante):
-            s.send(estudiante_seriado)
-            res=s.recv(1024)
+            self.s.send(estudiante_seriado)  # [PV] Usar socket de la clase self.s
+            res=self.s.recv(1024)  # [PV] Usar socket de la clase self.s
             res.decode()
             print(res)
-        else:
-            s.send(b'INI')
-            res= s.recv(1024)
-            print(res.decode())
 
-            if len(estudiante_seriado)<1024:
-                s.send(estudiante_seriado)
-
-                res=s.recv(1024)
-                print(res.decode())
-
-            else:
-                continuar = True
-                inicio=0
-                while continuar:
-                    chunk=estudiante_seriado[inicio::inicio+1024]
-
-                    if not chunk:
-                        continuar = False
-                        continue
-
-                    s.send(chunk)
-                    res = s.recv(1024)
-                    print(res.decode())
-                    inicio += 1024
-            s.send(b'FIN')
-            res=s.recv(1024)
-            print(res.decode())
-
-        res = s.recv(1024)
-        print(f'respuesta: \n\t{res.decode()}')
+        # [PV] La seccion comentada a continuacion es para el envio del archivo
+        # else:
+        #     self.s.send(b'INI')  # [PV] Usar socket de la clase self.s
+        #     res= self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        #     print(res.decode())
+        #
+        #     if len(estudiante_seriado)<1024:
+        #         self.s.send(estudiante_seriado)  # [PV] Usar socket de la clase self.s
+        #
+        #         res=self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        #         print(res.decode())
+        #
+        #     else:
+        #         continuar = True
+        #         inicio=0
+        #         while continuar:
+        #             chunk=estudiante_seriado[inicio::inicio+1024]
+        #
+        #             if not chunk:
+        #                 continuar = False
+        #                 continue
+        #
+        #             s.send(chunk)
+        #             res = s.recv(1024)
+        #             print(res.decode())
+        #             inicio += 1024
+        #     self.s.send(b'FIN')  # [PV] Usar socket de la clase self.s
+        #     res=self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        #     print(res.decode())
+        #
+        # res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        # print(f'respuesta: \n\t{res.decode()}')
         self.ui.enviarb.setEnabled(False)
         self.ui.buscarE.setEnabled(True)
         self.ui.buscarb.setEnabled(True)
@@ -137,52 +146,93 @@ class PaseLista(QMainWindow):
     def buscarArchivo(self):
         filename, _ =QFileDialog.getOpenFileName(file='*.zip')
         self.ui.buscarE.setText(filename)
+
+        # [PV] Si el nombre de archivo es correcto activar boton
+        if filename:
+            self.ui.archivosb.setEnabled(True)
+
     def rutaEnviar(self):
+
         if self.ui.buscarE.text():
             self.ui.archivosb.setEnabled(True)
 
         else:
             self.ui.archivosb.setEnabled(False)
-            s.send(b'INI')
-            res = s.recv(1024)
-            print(res.decode())
+            # self.s.send(b'INI')  # [PV] Usar socket de la clase self.s
+            # res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+            # print(res.decode())
+            #
+            # # [PV] Abrir archivo
+            # file = open(self.ui.buscarE.text(), 'rb')
+            #
+            # # [PV] Serializar archivo
+            # estudiante_seriado = pickle.dumps(file.read())
+            #
+            # if len(estudiante_seriado) < 1024:
+            #     self.s.send(estudiante_seriado)  # [PV] Usar socket de la clase self.s
+            #
+            #     res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+            #     print(res.decode())
+            #
+            # else:
+            #     continuar = True
+            #     inicio = 0
+            #     while continuar:
+            #         chunk = estudiante_seriado[inicio::inicio + 1024]
+            #
+            #         if not chunk:
+            #             continuar = False
+            #             continue
+            #
+            #         self.s.send(chunk)  # [PV] Usar socket de la clase self.s
+            #         res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+            #         print(res.decode())
+            #         inicio += 1024
+            # self.s.send(b'FIN')# [PV] Usar socket de la clase self.s
+            # res = self.s.recv(1024)# [PV] Usar socket de la clase self.s
+            # print(res.decode())
 
-            if len(estudiante_seriado) < 1024:
-                s.send(estudiante_seriado)
-
-                res = s.recv(1024)
-                print(res.decode())
-
-            else:
-                continuar = True
-                inicio = 0
-                while continuar:
-                    chunk = estudiante_seriado[inicio::inicio + 1024]
-
-                    if not chunk:
-                        continuar = False
-                        continue
-
-                    s.send(chunk)
-                    res = s.recv(1024)
-                    print(res.decode())
-                    inicio += 1024
-            s.send(b'FIN')
-            res = s.recv(1024)
-            print(res.decode())
-
-        res = s.recv(1024)
 
 
     def enviar(self):
         filename = self.ui.buscarE.text()
         file = open(filename, 'rb')
-        serializado= pickle.dumps(file)
-        s.send(serializado)
+        serializado= pickle.dumps(file.read()) # [PV] Se debe leer el contenido del archivo utilizando el metodo read
+        # self.s.send(serializado)  # [PV] Usar socket de la clase self.s
+
+        # [PV] Implementacion del protocolo de envio
+        self.s.send(b'INI')  # [PV] Usar socket de la clase self.s
+        res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        print(res.decode())
+
+
+        if len(serializado) < 1024:
+            self.s.send(serializado)  # [PV] Usar socket de la clase self.s
+
+            res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+            print(res.decode())
+
+        else:
+            continuar = True
+            inicio = 0
+            while continuar:
+                chunk = serializado[inicio::inicio + 1024]
+
+                if not chunk:
+                    continuar = False
+                    continue
+
+                self.s.send(chunk)  # [PV] Usar socket de la clase self.s
+                res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+                print(res.decode())
+                inicio += 1024
+        self.s.send(b'FIN')  # [PV] Usar socket de la clase self.s
+        res = self.s.recv(1024)  # [PV] Usar socket de la clase self.s
+        print(res.decode())
 
 
     def cerrarConexion (self):
-        s.close()
+        self.s.close()  # [PV] Usar socket de la clase self.s
         self.ui.desconectarb.setEnabled(False)
         self.ui.enviarb.setEnabled(True)
         self.ui.conectarb.setEnabled(False)
